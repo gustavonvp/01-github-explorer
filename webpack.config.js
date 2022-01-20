881844cc71fae298bee9f7f3398b78d27dc13acf
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2022-01-20 16:44:33 -0300

    Dev and Prod enviroments - Mapping Closes #10

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,13 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+
+
 module.exports =  {
-        mode: 'development',
-        devtool: 'eval-source-map',
+        mode: isDevelopment ?  'development' : 'production',
+        devtool: isDevelopment ? 'eval-source-map' : 'source-map',
         entry: [
             path.resolve(__dirname, 'src', 'index.jsx'),
             
