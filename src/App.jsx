4af9979d72b28f2ee193e6c5b7bfba6c52f6a17a
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2022-01-23 12:27:03 -0300

    interpolation and react states Closes #17

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import {render} from 'react-dom';
 import React, {Component} from 'react';
 import { RepositoryList } from './components/RepositoryList'
 import './styles/global.scss'
-
+import { Counter } from './components/Counter';
 export class App extends Component {
     render() {
             return(
+                <>
                 <RepositoryList />
+                <Counter />
+                </>
             )
     }
     
